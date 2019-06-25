39e7ca73ee88c583fdc2a5d37efaa901ec92aa1a
Lauren <Lauren@10.82.67.88>
2019-06-25 10:30:09 -0500

    Added comment and fixed issue with symbolic solving

--- method_of_moments.m
+++ method_of_moments.m
@@ -1,5 +1,8 @@
+%This code calcualtes the value of q for which the expected value of qe^-qt
+%is equal to the sample mean.
 function[q_mom]=method_of_moments(t)
 sample_mean=mean(t);
-expectation=1/q;
-q_mom=solve(expectation==sample_mean,q);
+%The expected value of qe^-qt is 1/q;
+syms q
+q_mom=solve((1/q)==sample_mean,q);
 end
