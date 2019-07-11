38bb7f4fd7e6c2e7466bfffc3a6afd2bbffcf560
John Ford <jrf2m@mtmail.mtsu.edu>
2019-07-11 14:59:27 -0500

    Factored optimize_jkn out of phospho_wrapper_compare
    Converted optimize_jkn to use local variables to the loop where possible
    
    Added parallelization to the num_samp_index loop

---
+++ optimize_jkn.m
@@ -0,0 +1,75 @@
+function [localstruct]=optimize_jkn(j,k,n_index,num_sims,num_samples,scale_small_probs,bw,q0,A,b,options,t,MLE_q_analytic)
+
+
+% CREATE A STRUCT HERE
+%data_cell{j,k,n_index} = struct();
+localstruct = struct();
+
+% ADD ALL OF THE APPROPRIATE ATTRIBUTE FIELDS
+localstruct.avg_ML_error = zeros(size(num_sims));
+localstruct.avg_MLE_q_approx_simulation = zeros(size(num_sims));
+localstruct.avg_approx_LL_simulation = zeros(size(num_sims));
+localstruct.sum_e_sample_squared = zeros(size(num_sims));
+
+for i=1:length(num_sims)
+    avg_ML_error = 0;
+    avg_MLE_q_approx_simulation = 0;
+    avg_approx_LL_simulation = 0;
+    sum_e_sample_squared = 0;
+    parfor num_samp_index=1:num_samples(i)
+
+        % Because we can underflow the probability, we need to
+        % sometimes replace the zero value with some degenerate
+        % nonzero probability to prevent the loglikeilhood from
+        % exploding. This value is scaled according to the number
+        % of simulations and the small probabillity scaling factor.
+        degenerate_probability = 1/(num_sims(i)*scale_small_probs(k));
+
+        % Prepare our objective function for optimization
+        negLL_approx=@(q)-1*likelihood_approx(t,q,num_sims(i),bw(j),degenerate_probability);
+
+        % Do the optimization and return the simulation MLE and LL
+        [q_sample,LL_sample] = fmincon(negLL_approx,q0,A,b,[],[],[],[],[],options);
+        LL_sample=-LL_sample;
+        e_sample=(abs(q_sample-(MLE_q_analytic(n_index))));
+
+        % avg_ML_error(i,j,k,n_index)=avg_ML_error(i,j,k,n_index)+e_sample;
+        avg_ML_error = avg_ML_error+e_sample;
+
+        % avg_MLE_q_approx_simulation(i,j,k,n_index)=avg_MLE_q_approx_simulation(i,j,k,n_index)+q_sample;
+        avg_MLE_q_approx_simulation = avg_MLE_q_approx_simulation+q_sample;
+
+        % avg_approx_LL_simulation(i,j,k,n_index)=avg_approx_LL_simulation(i,j,k,n_index)+LL_sample;
+        avg_approx_LL_simulation = avg_approx_LL_simulation+LL_sample;
+
+        %sum_e_sample_squared(i,j,k,n_index)=sum_e_sample_squared(i,j,k,n_index) +((e_sample)^2);
+        sum_e_sample_squared = sum_e_sample_squared+((e_sample)^2);
+
+        % Remember that we have to flip the LL back becaus the
+        % optimizer minimizes!
+
+    end
+    % avg_MLE_q_approx_simulation(i,j,k,n_index)=avg_MLE_q_approx_simulation(i,j,k,n_index)/num_samples(i);
+    avg_MLE_q_approx_simulation = avg_MLE_q_approx_simulation/num_samples(i);
+
+    % avg_approx_LL_simulation(i,j,k,n_index)=avg_approx_LL_simulation(i,j,k,n_index)/num_samples(i);
+    avg_approx_LL_simulation =  avg_approx_LL_simulation/num_samples(i);
+
+    % avg_ML_error(i,j,k,n_index)=avg_ML_error(i,j,k,n_index)/num_samples(i);
+    avg_ML_error = avg_ML_error/num_samples(i);
+
+    % not sure about this one
+    % sample_error_variance(i,j,k,n_index)=(sum_e_sample_squared(i,j,k,n_index)/num_samples(i))-((avg_ML_error(i,j,k,n_index))^2);
+    sample_error_variance = sum_e_sample_squared/num_samples(i)-((avg_ML_error)^2);
+
+
+    % HERE WE ASSIGN THE STRUCT TO OUR CELL ARRAY
+    localstruct.avg_ML_error(i) = avg_ML_error;
+    localstruct.avg_MLE_q_approx_simulation(i) = avg_MLE_q_approx_simulation;
+    localstruct.avg_approx_LL_simulation(i) = avg_approx_LL_simulation;
+    localstruct.sum_e_sample_squared(i) = sum_e_sample_squared;
+    localstruct.sample_error_variance(i) = sample_error_variance;
+end
+
+
+end
--- phospho_wrapper_compare.m
+++ phospho_wrapper_compare.m
@@ -9,7 +9,7 @@ function [MLE_q_numeric,MLE_q_analytic,mom_q,numeric_LL,Max_LL,q_LL,mom_LL,data_
 % Max_LL is the log likelihood of MLE_q_true
 % q_LL is the log likelihood of q
 
-gofast_mode=0;
+gofast_mode=1;
 
 % Make sure that the timestep, called h in other places, is always
 % infintestimal compared to our provided value for q
@@ -46,21 +46,7 @@ else
     num_samples=10*ones(size(num_sims));
 end
 
-% WE WILL GET RID OF THIS PREALLOCATION STUFF
 
-% Preallocate the matrices holding the MLE and associated LL for each
-% simulation run.
-
-% avg_MLE_q_approx_simulation=zeros(length(num_sims),length(bw),length(scale_small_probs),length(data_nums));
-% avg_approx_LL_simulation=zeros(length(num_sims),length(bw),length(scale_small_probs),length(data_nums));
-% avg_ML_error=zeros(length(num_sims),length(bw),length(scale_small_probs),length(data_nums));
-% sum_e_sample_squared=zeros(length(num_sims),length(bw),length(scale_small_probs),length(data_nums));
-% sample_error_variance=zeros(length(num_sims),length(bw),length(scale_small_probs),length(data_nums));
-
-% Loop through our desired number of samples, indexed by n_index,
-% data_nums(n_index)
-
-% BY REPLACING IT WITH A SINGLE CELL ARRAY PREALLOCATION HERE
 data_cell = cell(length(bw),length(scale_small_probs),length(data_nums));
 
 MLE_q_numeric=zeros(size(data_nums));
@@ -117,113 +103,23 @@ for n_index=1:length(data_nums)
     plot(q_values,likelihood(t,q_values));
     %saveas(gcf,'likelihood_plot');
     
-    
-    
     % Loop through our desired number of simulations, indexed by i,
     % num_sims(i)
     
-   
-
     % Loop through our desired bin widths, indexed by j, bw(j)
+    
+    
+    
+    
     for j=1:length(bw)
-
         % Loop through our desired small probability scaling factors,
         % indexed by k, scale_small_probs(k)
-        parfor k=1:length(scale_small_probs)
-            
-            % CREATE A STRUCT HERE
-                 %data_cell{j,k,n_index} = struct();
-                 localstruct = struct();
-                 
-                % ADD ALL OF THE APPROPRIATE ATTRIBUTE FIELDS
-                localstruct.avg_ML_error = zeros(size(num_sims));
-                localstruct.avg_MLE_q_approx_simulation = zeros(size(num_sims));
-                localstruct.avg_approx_LL_simulation = zeros(size(num_sims));
-                localstruct.sum_e_sample_squared = zeros(size(num_sims));
-                
-                
-
-             for i=1:length(num_sims)
-                 
-               
-                for num_samp_index=1:num_samples(i)
-                    
-                    
-                    
-                    
-
-                    % Because we can underflow the probability, we need to
-                    % sometimes replace the zero value with some degenerate
-                    % nonzero probability to prevent the loglikeilhood from
-                    % exploding. This value is scaled according to the number
-                    % of simulations and the small probabillity scaling factor.
-                    degenerate_probability = 1/(num_sims(i)*scale_small_probs(k));
-                   
-
-                    % Prepare our objective function for optimization
-                    negLL_approx=@(q)-1*likelihood_approx(t,q,num_sims(i),bw(j),degenerate_probability);
-
-                    % Do the optimization and return the simulation MLE and LL
-                    [q_sample,LL_sample] = fmincon(negLL_approx,q0,A,b,[],[],[],[],[],options);
-                    LL_sample=-LL_sample;
-                    e_sample=(abs(q_sample-(MLE_q_analytic(n_index))));
-                    
-                    
-                    
-                    
-                    
-                    % avg_ML_error(i,j,k,n_index)=avg_ML_error(i,j,k,n_index)+e_sample;
-                    localstruct.avg_ML_error(i) = localstruct.avg_ML_error(i)+e_sample;
-
-                    
-                    % avg_MLE_q_approx_simulation(i,j,k,n_index)=avg_MLE_q_approx_simulation(i,j,k,n_index)+q_sample;
-                   localstruct.avg_MLE_q_approx_simulation(i) = localstruct.avg_MLE_q_approx_simulation(i)+q_sample;
-                    
-                    % avg_approx_LL_simulation(i,j,k,n_index)=avg_approx_LL_simulation(i,j,k,n_index)+LL_sample;
-                     localstruct.avg_approx_LL_simulation(i) = localstruct.avg_approx_LL_simulation(i)+LL_sample;
-
-                    %sum_e_sample_squared(i,j,k,n_index)=sum_e_sample_squared(i,j,k,n_index) +((e_sample)^2);
-                    localstruct.sum_e_sample_squared(i) = localstruct.sum_e_sample_squared(i)+((e_sample)^2);
-                    
-                    % Remember that we have to flip the LL back becaus the
-                    % optimizer minimizes!
-
-                end
-                % avg_MLE_q_approx_simulation(i,j,k,n_index)=avg_MLE_q_approx_simulation(i,j,k,n_index)/num_samples(i);
-                localstruct.avg_MLE_q_approx_simulation(i) = localstruct.avg_MLE_q_approx_simulation(i)/num_samples(i);
-                
-                % avg_approx_LL_simulation(i,j,k,n_index)=avg_approx_LL_simulation(i,j,k,n_index)/num_samples(i);
-                localstruct.avg_approx_LL_simulation(i) =  localstruct.avg_approx_LL_simulation(i)/num_samples(i);
-                
-                % avg_ML_error(i,j,k,n_index)=avg_ML_error(i,j,k,n_index)/num_samples(i);
-               localstruct.avg_ML_error(i) = localstruct.avg_ML_error(i)/num_samples(i);
-                
-                % not sure about this one
-                % sample_error_variance(i,j,k,n_index)=(sum_e_sample_squared(i,j,k,n_index)/num_samples(i))-((avg_ML_error(i,j,k,n_index))^2);
-                 localstruct.sample_error_variance(i) = localstruct.sum_e_sample_squared(i)/num_samples(i)-((localstruct.avg_ML_error(i))^2);
-                
-                
-                % HERE WE ASSIGN THE STRUCT TO OUR CELL ARRAY
-                
-             end
-             
-             data_cell{j,k,n_index} = localstruct; 
+        for k=1:length(scale_small_probs)
+            [localstruct]=optimize_jkn(j,k,n_index,num_sims,num_samples,scale_small_probs,bw,q0,A,b,options,t,MLE_q_analytic)
+            data_cell{j,k,n_index} = localstruct;     
         end
     end
-    
-   
 end
-
-
-% MOVE ALL THE SUMMATION DOWN HERE
-
-
-
-
-% END OF SUMMATION BLOCK
-
-
-
 
 end
  
