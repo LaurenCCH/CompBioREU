ef80e6f3bd0afee64eae39cc8496d4d4af954365
Amirhossein  Samiepour <as2ey@mtmail.mtsu.edu>
2019-07-12 14:22:44 -0500

    compared two curve fits

--- compare_plots.m
+++ compare_plots.m
@@ -6,7 +6,9 @@ for i=1:length(bw)
    for j=1:length(scale_small_probs)
         for k=1:length(data_nums)
            
-            [p,k_const,SSE]=convergence_rate(num_sims,data_cell{i,j,k}.avg_ML_error);
+            [p_k,k_const,SSE_k]=convergence_rate(num_sims,data_cell{i,j,k}.avg_ML_error);
+            [c,p,SSE_c] = convergence_rate_C(data_cell{i,j,k}.avg_ML_error,num_sims);
+
             
             set(figure,'DefaultFigureWindowStyle','docked')
             hold on
@@ -16,10 +18,15 @@ for i=1:length(bw)
             ylabel('MLE error and MLE variance') 
             plot(num_sims,data_cell{i,j,k}.sample_error_variance,'o')
             fine_range_mesh=num_sims(end):(.01):num_sims(1);
-         
-            plot(fine_range_mesh, (k_const./(fine_range_mesh).^p))
-            annotation('textbox','String',sprintf("p=%f\nk\\_const=%f\nSSE=%f",p,k_const,SSE),'FitBoxToText','on');
-            legend('avg\_ML\_error','sample\_error\_variance','decay fit');
+            
+            %This is the plot for convergence_rate
+            plot(fine_range_mesh, (k_const./(fine_range_mesh).^p_k),'r')
+            
+            plot(fine_range_mesh, (1./(fine_range_mesh).^p)+c,'b')
+            %This annotation is for convergence_rate
+            annotation('textbox','String',sprintf("p=%f\nk\\_const=%f\nSSE=%f",p_k,k_const,SSE_k),'FitBoxToText','on');
+            annotation('textbox','String',sprintf("p=%f\nc=%f\nSSE=%f",p,c,SSE_c),'FitBoxToText','on');
+            legend('avg\_ML\_error','sample\_error\_variance','decay fit k','decay fit c');
         end
     end
 end
