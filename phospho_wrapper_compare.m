e1f11ed01188b1dc6957dac14a67fd1064464d73
as2ey <as2ey@D003843KOM204.fsa.mtsu.edu>
2019-07-16 16:08:43 -0500

    changed it to slowmode

--- phospho_wrapper_compare.m
+++ phospho_wrapper_compare.m
@@ -9,7 +9,7 @@ function [MLE_q_numeric,MLE_q_analytic,mom_q,numeric_LL,Max_LL,q_LL,mom_LL,data_
 % Max_LL is the log likelihood of MLE_q_true
 % q_LL is the log likelihood of q
 
-gofast_mode=1;
+gofast_mode=0;
 
 % Make sure that the timestep, called h in other places, is always
 % infintestimal compared to our provided value for q
@@ -38,12 +38,18 @@ if(gofast_mode==1)
     
 else
     % This is the go slow, full mode
+%     data_nums = [1000,500, 250,100, 50];
+%     num_sims=[50 250 500 750 1000 2500 5000 7500 10000];
+%     num_sims=fliplr(num_sims);
+%     bw = [timestep*2, timestep*5, timestep*10,timestep*20, timestep*30, timestep*40,timestep*50, timestep*60, timestep*70, timestep*80, timestep*90, timestep*100];
+%     scale_small_probs=[10,20, 30, 40, 50, 60, 70, 80, 90 100];
+%     num_samples=50;
     data_nums = [1000,500, 250,100, 50];
     num_sims=[50 250 500 750 1000 2500 5000 7500 10000];
     num_sims=fliplr(num_sims);
-    bw = [timestep*2, timestep*5, timestep*10,timestep*20, timestep*30, timestep*40,timestep*50, timestep*60, timestep*70, timestep*80, timestep*90, timestep*100];
-    scale_small_probs=[10,20, 30, 40, 50, 60, 70, 80, 90 100];
-    num_samples=10;
+    bw = [timestep*2, timestep*10, timestep*100];
+    scale_small_probs=[10,100];
+    num_samples=50;
 end
 
 
