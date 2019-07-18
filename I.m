ef80e3d0aa6759f3a39fd538e2839c1cba45d933
Lauren <Lauren@10.82.67.88>
2019-07-18 15:13:05 -0500

    Changed variable names to be easier to understand, changed the hard-coded coefficient of variation to be dependent on bw from phospho_wrapper_compare, changed structure of I and approx_likelihood_kernel

--- I.m
+++ I.m
@@ -1,5 +1,15 @@
-function[x_hats_to_sum]=I(x, x_i_hat)
-prob_search=find (abs(x-x_i_hat)<.01*2*sqrt(3)*x_i_hat);
-how_many_x_hats=size(prob_search);
-x_hats_to_sum=((1/(.01*2*sqrt(3)*x_hat))*ones(how_many_x_hats));
+function[density]=I(sim_data, exp_data_i, cv)
+%evaluate the uniform kernel centered at x_hat at the data point x_i
+
+%the bin width is chosen so that the ratio of the std dev to the mean is
+%fixed at .01
+
+bin_width=cv*2*sqrt(3)*sim_data;
+density=0;
+%if exp_data_i falls into the bin the density of the kernel at exp_data_i
+%is the height of the bin, 1/bin_width. Otherwise, the density of the
+%kernel at exp_data_i is 0.
+if abs(sim_data-exp_data_i)<bin_width
+    density=1/bin_width;
+end
 end
---
+++ approx_likelihood_kernel.m
@@ -0,0 +1,29 @@
+function[LL] = approx_likelihood_kernel(data, num_sims, q_hat,degenerate_prob, bw)
+%This code approximates the log likelihood of given data (data).  
+%Specifically, we use simulated data (sim_data) to generate an approximate 
+%pdf based on the kernel provided by I. We calculate the LL of the given
+%data provided the approximate pdf.
+
+%For simulating the continous stochastic process we choose the time step to
+%be small relative to the occurrence rate, q.
+h = 1/(q_hat*100);
+cv=1/bw;
+%simulated data for generating the approximate pdf
+[sim_data] = phospho_times(q_hat,h,num_sims);
+%the approximate pdf value of each point in data is stored in pdf_value
+pdf_value=zeros(1,length(data));
+%loop through data values to get corresponding approximate pdf values
+for data_index=1:length(data)
+    %loop through the kernels centered on the simulated data to find the
+    %density each kernel contributes to pdf_value(data_index). These values
+    %sum to give the final pdf_value(data_index).
+    for sim_data_index=1:num_sims
+        pdf_value(data_index) = pdf_value(data_index)+I(sim_data(sim_data_index), data(data_index), cv);
+    end
+    pdf_value(data_index)=pdf_value(data_index)/(num_sims);
+    if pdf_value(data_index)==0
+        pdf_value(data_index)=degenerate_prob;
+    end
+end
+LL=sum(log(pdf_value));
+end
